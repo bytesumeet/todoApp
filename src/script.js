74eb7da775479fa0510f28b8d92446611fb7fbb3
sumit pal <debuggersumit@gmail.com>
2026-07-27 21:26:53 +0530

    set up DOM selectors and DOMContentLoaded entry point

---
+++ src/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const addTaskFormElement = document.querySelector("#add-todo-form");
+	const todoInputElement = document.querySelector("#todo-input");
+	const searchInputElement = document.querySelector("#search-input");
+	const clearAllButtonElement = document.querySelector("#clear-all-btn");
+	const taskListElement = document.querySelector("#todo-list");
+	const emptyStateElement = document.querySelector("#empty-state");
+	
+})
